f200cd5a107a6fb156b3b40cf02c3e06abfe9c37
Nick Duncan <nick.b.duncan@gmail.com>
2015-12-30 16:22:34 -0800

    Example function to construct random sentences based on the passed in JSON file.

--- test/test.js
+++ test/test.js
@@ -1,30 +1,43 @@
-var rhyme = require('rhyme-plus');
-var gg = require('grammar-graph');
+/*
+A testing file for how we can construct sentences.
+*/
+var rhyme = require('rhyme-plus')
+, gg = require('grammar-graph')
+, JSONStream = require('JSONStream')
+, fs = require('fs')
+, es = require('event-stream')
 
-var grammar = {
-  Sentence: ['NounPhrase VerbPhrase'],
-  NounPhrase: [
-    'the Noun',
-    'the Noun RelativeClause'
-  ],
-  VerbPhrase: [
-    'Verb',
-    'Verb NounPhrase'
-  ],
-  RelativeClause: ['that VerbPhrase'],
-  Noun: [
-    'dog',
-    'cat',
-    'bird',
-    'squirrel'
-  ],
-  Verb: [
-    'befriended',
-    'loved',
-    'ate',
-    'attacked'
-  ]
+var generateSentencePool = (grammar) => {
+
+  var guide = new gg(grammar).createGuide('Sentence')
+  , recog = new gg(grammar).createRecognizer('Sentence')
+  , currSentence = '';
+
+  while (!recog.isComplete(currSentence)) {
+    guide.choose(
+      guide.choices()[
+        Math.floor(Math.random()*guide.choices().length)
+      ]
+    );
+    
+    currSentence = guide.constructs()[
+      Math.floor(Math.random()*guide.constructs().length)
+    ];
+  }
+
+  return currSentence;
 }
 
-var graph = new gg(grammar);
-console.log(graph.vertices());
+var parser = JSONStream.parse()
+, sentenceGenerator = es.mapSync((data) => {
+  var x = generateSentencePool(data)
+  console.log(x);
+  return x;
+})
+var rStream = fs.createReadStream(
+  __dirname + '/res/sentenceRules.json'
+).pipe(
+  parser
+).pipe(
+  sentenceGenerator
+);
