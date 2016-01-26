9159b5390940fb560d4f4cea24e42412c0ded611
Nick Duncan <nick.b.duncan@gmail.com>
2016-01-26 12:09:24 -0800

    Whoops, last commit didn't include the check.

--- components/wordnikresponseparse.js
+++ components/wordnikresponseparse.js
@@ -15,36 +15,38 @@ module.exports = {
         };
 
         for (var i = 0; i < res.length; i++) {
-            switch (res[i].relationshipType) {
+            if (res[i].relationshipType) {
+                switch (res[i].relationshipType) {
 
-                case 'equivalent' :
-                    relationships.equivalent = res[i].words;
-                    break;
-                case 'hypernym' :
-                    relationships.hypernym = res[i].words;
-                    break;
-                case 'cross-reference':
-                    relationships.crossReference = res[i].words;
-                    break;
-                case 'etymologically-related-term' :
-                    relationships.etymologicallyRelatedTerm = res[i].words;
-                    break;
-                case 'synonym' :
-                    relationships.synonym = res[i].words;
-                    break;
-                case 'rhyme' :
-                    relationships.rhyme = res[i].words;
-                    break;
-                case 'unknown' :
-                    relationships.unknown = res[i].words;
-                    break;
-                case 'same-context' :
-                    relationships.sameContext = res[i].words;
-                    break;
+                    case 'equivalent' :
+                        relationships.equivalent = res[i].words;
+                        break;
+                    case 'hypernym' :
+                        relationships.hypernym = res[i].words;
+                        break;
+                    case 'cross-reference':
+                        relationships.crossReference = res[i].words;
+                        break;
+                    case 'etymologically-related-term' :
+                        relationships.etymologicallyRelatedTerm = res[i].words;
+                        break;
+                    case 'synonym' :
+                        relationships.synonym = res[i].words;
+                        break;
+                    case 'rhyme' :
+                        relationships.rhyme = res[i].words;
+                        break;
+                    case 'unknown' :
+                        relationships.unknown = res[i].words;
+                        break;
+                    case 'same-context' :
+                        relationships.sameContext = res[i].words;
+                        break;
+                }
+
             }
-
+            return relationships;
         }
-        return relationships;
     }
 
 };
