ee9ebc820c4bfc5b589c0ff206602a3f3a47e311
IntegralOfZero <constantpham@outlook.com>
2016-01-28 14:27:09 -0800

    Small grammar tweaks, accounted for modal verbs in conjugation present tense verbs by person

--- components/sentencebuilder.js
+++ components/sentencebuilder.js
@@ -59,32 +59,31 @@ var wordTypes = {
 var wordTypeDictionary = {};
 
 var constructSentences = (words, grammar, numberOfSentences) => {
-  var constructedSentences = [];
 
-  grammar.RhymingWords = words.rhymingWords;
-  grammar.Noun = grammar.Noun.concat(words.nouns);
-  grammar.Verb = grammar.Verb.concat(words.verbs);
-  grammar.Adjective = grammar.Adjective.concat(words.adjectives);
-  grammar.Adverb = grammar.Adverb.concat(words.adverbs);
-  if (grammar.Rest) grammar.Rest = words.Rest;
-  grammar.TopicWord = words.topic;
-  var conjugatedVerbs = conjugateVerbs(words.verbs);
-  grammar.VerbPast = conjugatedVerbs.past;
-  grammar.VerbPerfect = conjugatedVerbs.perfect;
-  grammar.VerbPresent = conjugatedVerbs.present;
-  grammar.VerbProgressive = conjugatedVerbs.progressive;
-/*
-grammar.NounRhyming = 
-  grammar.AdjectiveRhyming =
-  grammar.AdverbRhyming =
-  grammar.LocativeAdverbRhyming
-  grammar.VerbPastRhyming =
-  grammar.VerbPerfectRhyming =
-  grammar.VerbPresentRhyming =
-  grammar.VerbProgressiveRhyming =
-*/
-  //clear it before each use
-  wordTypeDictionary = {};
+    grammar.RhymingWords = words.rhymingWords;
+    grammar.Noun = grammar.Noun.concat(words.nouns);
+    grammar.Verb = grammar.Verb.concat(words.verbs);
+    grammar.Adjective = grammar.Adjective.concat(words.adjectives);
+    grammar.Adverb = grammar.Adverb.concat(words.adverbs);
+    if (grammar.Rest) grammar.Rest = words.Rest;
+    grammar.TopicWord = words.topic;
+    var conjugatedVerbs = conjugateVerbs(words.verbs);
+    grammar.VerbPast = conjugatedVerbs.past;
+    grammar.VerbPerfect = conjugatedVerbs.perfect;
+    grammar.VerbPresent = conjugatedVerbs.present;
+    grammar.VerbProgressive = conjugatedVerbs.progressive;
+    grammar.NounRhyming = words.NounRhyming;
+    grammar.AdjectiveRhyming = words.AdjectiveRhyming;
+    grammar.AdverbRhyming = words.AdverbRhyming;
+    var conjugatedRhymeVerbs = conjugateVerbs(words.VerbRhyming);
+    //grammar.LocativeAdverbRhyming
+    grammar.VerbPastRhyming = conjugatedRhymeVerbs.past;
+    grammar.VerbPerfectRhyming = conjugatedRhymeVerbs.perfect;
+    grammar.VerbPresentRhyming = conjugatedRhymeVerbs.present;
+    grammar.VerbProgressiveRhyming = conjugatedRhymeVerbs.progressive;
+
+    //clear it before each use
+    wordTypeDictionary = {};
 
   addWordsToDictionary(wordTypeDictionary, grammar.Noun, wordTypes.Noun);
   addWordsToDictionary(wordTypeDictionary, grammar.PronounNominative, wordTypes.PronounNominative);
@@ -101,15 +100,16 @@ grammar.NounRhyming =
   addWordsToDictionary(wordTypeDictionary, grammar.DeterminerPlural, wordTypes.DeterminerPlural);
   addWordsToDictionary(wordTypeDictionary, grammar.DeterminerSingular, wordTypes.DeterminerSingular);
   addWordsToDictionary(wordTypeDictionary, grammar.AuxiliaryVerbBe, wordTypes.AuxiliaryVerbBe);
-  /*
+
   addWordsToDictionary(wordTypeDictionary, grammar.NounRhyming, wordTypes.NounRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.AdjectiveRhyming, wordTypes.AdjectiveRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.AdverbRhyming, wordTypes.AdverbRhyming);
-  addWordsToDictionary(wordTypeDictionary, grammar.LocativeAdverbRhyming, wordTypes.LocativeAdverbRhyming);
+//addWordsToDictionary(wordTypeDictionary, grammar.LocativeAdverbRhyming, wordTypes.LocativeAdverbRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.VerbPastRhyming, wordTypes.VerbPastRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.VerbPerfectRhyming, wordTypes.VerbPerfectRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.VerbPresentRhyming, wordTypes.VerbPresentRhyming);
   addWordsToDictionary(wordTypeDictionary, grammar.VerbProgressiveRhyming, wordTypes.VerbProgressiveRhyming);
+  addWordsToDictionary(wordTypeDictionary, grammar.ModalVerb, wordTypes.ModalVerb);
 
 
 /*
@@ -122,66 +122,99 @@ VerbPerfectRhyming
 VerbPresentRhyming
 VerbProgressiveRhyming
 */
+    var sentencesToBeSentToVerses = [];
+    for (var i = 0; i < 8; i++) {
+        //concat two sentences to the final list of sentences that will total 16.
+        sentencesToBeSentToVerses.concat(
+            generateTwoSentences(grammar)
+        );
+        //reset topicword
+        grammar.TopicWord[0] = chooseANewTopic(grammar.Noun);
+        // get rhymingwords for new topic
 
-  var recognizer = new gg(grammar).createRecognizer(completeSentenceChoice);
-
-  while (constructedSentences.length < numberOfSentences) {
-    var currSentence = '';
-    var outputSentence = '';
-    var guide = new gg(grammar).createGuide(completeSentenceChoice);
-
-    //for handling various inflection rules
-    var previousWord = '';
-    var previousDeterminer = '';
-
-    while (!recognizer.isComplete(currSentence)) {
-      var choice = guide.choices()[Math.floor(Math.random()*guide.choices().length)];
-
-      //progress through the graph, call choose() prior to inflection since it may not be a valid choice afterward
-      guide.choose(choice);
-
-      if(wordTypeDictionary.hasOwnProperty(choice) && wordTypeDictionary.hasOwnProperty(previousWord)) {
-        var types = wordTypeDictionary[choice];
-        if(types.indexOf(wordTypes.DeterminerSingular) > -1 || types.indexOf(wordTypes.DeterminerPlural) > -1) {
-          previousDeterminer = choice;
-        }
-        if((types.indexOf(wordTypes.Noun) > -1 || types.indexOf(wordTypes.NounRhyming) > -1) && previousDeterminer.length > 0) {
-          choice = declineNounByNumber(choice, previousDeterminer);
-        }
-        else if(types.indexOf(wordTypes.VerbPresent) > -1 || types.indexOf(wordTypes.VerbPresentRhyming) > -1) {
-          choice = conjugateVerbPresentByPerson(choice, previousWord);
-        }
-        else if(types.indexOf(wordTypes.AuxiliaryVerbBe) > -1) {
-          choice = conjugateAuxiliaryVerbBeByPerson(choice, previousWord);
-        }
-      }
-
-      //build the sentence being generated one word at a time
-      outputSentence += choice + ' ';
-
-      //this doesn't actually select another path, but rather ensures that the graph goes to the end of a possible path
-      currSentence = guide.constructs()[Math.floor(Math.random()*guide.constructs().length)];
-
-      previousWord = choice;
+        // get pos types for new rhyming words
+        // re-inject the grammar
     }
+    return sentencesToBeSentToVerses;
+};
 
-    //make sure to get the last word of the generated sentence
-    var lastWord = currSentence.split(' ').pop(); 
+var generateTwoSentences = (grammar) => {
+    var constructedSentences = [];
 
-/*
-    //just go with the first type if it has more than one
-    var typeOfLastWord = wordTypeDictionary[lastWord][0];
-    typeOfLastWord += 'Rhyming';
+    var recognizer = new gg(grammar)
+        .createRecognizer(
+            completeSentenceChoice
+        );
 
-    //the last word in the sentence will be one that rhymes but is the same part of speech as the one it replaces
-    lastWord = grammar[typeOfLastWord][Math.floor(Math.random()*grammar[typeOfLastWord].length)]
-*/
+    while (constructedSentences.length < 2) {
+        var currSentence = '';
+        var outputSentence = '';
+        var guide = new gg(grammar).createGuide(completeSentenceChoice);
 
-    outputSentence += lastWord;
-    constructedSentences.push(outputSentence);
-  }
+        //for handling various inflection rules
+        var previousWord = '';
+        var previousDeterminer = '';
+        var modalPresent = false;
 
-  return constructedSentences;
+        while (!recognizer.isComplete(currSentence)) {
+            var choice = guide.choices()[Math.floor(Math.random()*guide.choices().length)];
+
+            //progress through the graph, call choose() prior to inflection since it may not be a valid choice afterward
+            guide.choose(choice);
+
+            if(wordTypeDictionary.hasOwnProperty(choice) && wordTypeDictionary.hasOwnProperty(previousWord)) {
+                var types = wordTypeDictionary[choice];
+
+                if(types.indexOf(wordTypes.DeterminerSingular) > -1 || types.indexOf(wordTypes.DeterminerPlural) > -1) {
+                    previousDeterminer = choice;
+                }
+                if(types.indexOf(wordtypes.ModalVerb) > -1) {
+                    modalPresent = true;
+                } 
+
+                if((types.indexOf(wordTypes.Noun) > -1 || types.indexOf(wordTypes.NounRhyming) > -1) && previousDeterminer.length > 0) {
+                    choice = declineNounByNumber(choice, previousDeterminer);
+                }
+                else if(types.indexOf(wordTypes.VerbPresent) > -1 || types.indexOf(wordTypes.VerbPresentRhyming) > -1) {
+                    choice = conjugateVerbPresentByPerson(choice, previousWord, modalPresent);
+                    if(modalPresent) {
+                      modalPresent = false;
+                    }
+                }
+                else if(types.indexOf(wordTypes.AuxiliaryVerbBe) > -1) {
+                    choice = conjugateAuxiliaryVerbBeByPerson(choice, previousWord);
+                }
+            }
+
+            //build the sentence being generated one word at a time
+            outputSentence += choice + ' ';
+
+            //this doesn't actually select another path, but rather ensures that the graph goes to the end of a possible path
+            currSentence = guide.constructs()[Math.floor(Math.random()*guide.constructs().length)];
+
+            previousWord = choice;
+        }
+        //make sure to get the last word of the generated sentence
+        var lastWord = currSentence.split(' ').pop();
+
+        /*
+         //just go with the first type if it has more than one
+         var typeOfLastWord = wordTypeDictionary[lastWord][0];
+         typeOfLastWord += 'Rhyming';
+
+         //the last word in the sentence will be one that rhymes but is the same part of speech as the one it replaces
+         lastWord = grammar[typeOfLastWord][Math.floor(Math.random()*grammar[typeOfLastWord].length)]
+         */
+        outputSentence += lastWord;
+        constructedSentences.push(outputSentence);
+    }
+    return constructedSentences;
+};
+
+var chooseANewTopic = (relatedNouns) => {
+
+    var chosenIndex = Math.floor(Math.random()*relatedNouns.length);
+    return relatedNouns.splice(chosenIndex, 1);
 };
 
 var conjugateVerbs = (verbs) => {
@@ -226,14 +259,14 @@ var declineNounByNumber = (word, previousDeterminer) => {
   return word;
 };
 
-var conjugateVerbPresentByPerson = (word, previousWord) => {
+var conjugateVerbPresentByPerson = (word, previousWord, modalPresent) => {
   var nonThirdPersonPronouns = ['I', 'you', 'we', 'they'];
-  if(nonThirdPersonPronouns.indexOf(previousWord) > -1) {
+  if(nonThirdPersonPronouns.indexOf(previousWord) > -1 || modalPresent) {
     console.log("CONJUGATE VP REST: " + word);
     return conjugate('you', word);
   }
   console.log("CONJUGATE VP 3rd P: " + word);
-  return conjugate('you', word);
+  return conjugate('it', word);
 };
 
 var conjugateAuxiliaryVerbBeByPerson = (word, previousWord) => {
